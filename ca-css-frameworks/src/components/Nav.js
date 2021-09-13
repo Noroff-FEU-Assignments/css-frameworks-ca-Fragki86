ff6067b450e1f8f6d6bae93f9d83e4cf76916d86
Georgios Fragkias <geo.fragkias@gmail.com>
2021-09-13 15:32:51 +0200

    2nd

--- ca-css-frameworks/src/components/Nav.js
+++ ca-css-frameworks/src/components/Nav.js
@@ -7,20 +7,23 @@ function Nav() {
         <Navbar bg="light" expand="lg">
           <Container>
             <Navbar.Brand href="#home">The YAY company</Navbar.Brand>
-            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
+        <Nav.Link href="#link">News</Nav.Link>
+        <Nav.Link href="#link">Contact</Nav.Link>
       </Nav>
-    </Navbar.Collapse> */}
+      <Form className="d-flex">
+      <FormControl
+        type="search"
+        placeholder="Search"
+        className="mr-2"
+        aria-label="Search"
+      />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+    </Navbar.Collapse>
   </Container>
 </Navbar>
     );
