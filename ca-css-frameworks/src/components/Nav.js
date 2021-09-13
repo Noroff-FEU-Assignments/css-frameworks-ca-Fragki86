b537d673fd76c4adcb755fd6ebadb064a0d6b3f1
Georgios Fragkias <geo.fragkias@gmail.com>
2021-09-13 15:39:16 +0200

    2nd

--- ca-css-frameworks/src/components/Nav.js
+++ ca-css-frameworks/src/components/Nav.js
@@ -1,6 +1,9 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+// import FormControl from "react-bootstrap/FormControl";
 
 function Nav() {
     return (
@@ -11,16 +14,16 @@ function Nav() {
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">News</Nav.Link>
-        <Nav.Link href="#link">Contact</Nav.Link>
+        <Nav.Link href="#news">News</Nav.Link>
+        <Nav.Link href="#contact">Contact</Nav.Link>
       </Nav>
       <Form className="d-flex">
-      <FormControl
+      {/* <FormControl
         type="search"
         placeholder="Search"
         className="mr-2"
         aria-label="Search"
-      />
+      /> */}
       <Button variant="outline-success">Search</Button>
     </Form>
     </Navbar.Collapse>
